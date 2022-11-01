d5973a4718a921210f52888331c86d28afbe9b19
mddsall <mddieng14@gmail.com>
2022-11-01 13:39:51 +0000

    crud sess-formation

--- src/main/webapp/app/entities/session-form/route/session-form-routing.module.ts
+++ src/main/webapp/app/entities/session-form/route/session-form-routing.module.ts
@@ -14,7 +14,7 @@ const sessionFormRoute: Routes = [
     data: {
       defaultSort: 'id,asc',
     },
-    canActivate: [UserRouteAccessService],
+    // canActivate: [UserRouteAccessService],
   },
   {
     path: ':id/view',
