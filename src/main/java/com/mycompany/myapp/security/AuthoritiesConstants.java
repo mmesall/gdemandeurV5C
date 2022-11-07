40fd927f44d16b6fea6b185cbabaef69e436bc6b
mddsall <mddieng14@gmail.com>
2022-11-07 16:28:58 +0000

    authority

--- src/main/java/com/mycompany/myapp/security/AuthoritiesConstants.java
+++ src/main/java/com/mycompany/myapp/security/AuthoritiesConstants.java
@@ -11,5 +11,15 @@ public final class AuthoritiesConstants {
 
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
 
+    public static final String AGENT = "ROLE_AGENT";
+
+    public static final String DEMANDEUR = "ROLE_DEMANDEUR";
+
+    public static final String DRH = "ROLE_DRH";
+
+    public static final String DECPC = "ROLE_DECPC";
+
+    public static final String BAILLEUR = "ROLE_BAILLEUR";
+
     private AuthoritiesConstants() {}
 }
